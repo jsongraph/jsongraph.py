a856bde9bc378d94a6527537e47084d408e1f07d
William Hayes <william.s.hayes@gmail.com>
2020-07-09 08:41:16 -0400

    Updated example jsongraph3.py

--- jsongraph3.py
+++ jsongraph3.py
@@ -14,23 +14,26 @@ None
 Long Description
 """
 
+import io
 import json
 import os.path
-from jsonschema import Draft4Validator
-import sys
 import ssl
-import io
-import urllib.request, urllib.parse, urllib.error
-from objects.multigraph import Multigraph
-from objects.graph import Graph
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+
+from jsonschema import Draft4Validator
 from objects.edge import Edge
-from objects.node import Node #TODO add in original
+from objects.graph import Graph
+from objects.multigraph import Multigraph
+from objects.node import Node  # TODO add in original
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def load_json_string(jsonstring):
-    '''Check if string is JSON and if so return python dictionary'''
+    """Check if string is JSON and if so return python dictionary"""
 
     try:
         json_object = json.loads(jsonstring)
@@ -40,8 +43,8 @@ def load_json_string(jsonstring):
 
 
 def get_github_masterschema():
-    '''Read JSON Graph Schema file from Github Master branch'''
-    link = 'https://raw.githubusercontent.com/jsongraph/json-graph-specification/master/json-graph-schema.json'
+    """Read JSON Graph Schema file from Github Master branch"""
+    link = "https://raw.githubusercontent.com/jsongraph/json-graph-specification/master/json-graph-schema_v2.json"
     f = urllib.request.urlopen(link)
     js = json.load(f)
     f.close
@@ -50,16 +53,16 @@ def get_github_masterschema():
 
 
 def get_json(jsongraph):
-    '''Check if parameter is a file object, filepath or JSON string
+    """Check if parameter is a file object, filepath or JSON string
 
         Return:  dictionary object OR False for failure
-    '''
+    """
     if type(jsongraph) is dict:
         return jsongraph
     elif isinstance(jsongraph, io.IOBase):
         return json.load(jsongraph)
     elif os.path.isfile(jsongraph):
-        with open(jsongraph, 'rb') as f:
+        with open(jsongraph, "rb") as f:
             return json.load(f)
 
     jg = load_json_string(jsongraph)
@@ -69,8 +72,8 @@ def get_json(jsongraph):
         return False
 
 
-def validate_schema(schema='', verbose=False):
-    '''Validate schema file'''
+def validate_schema(schema="", verbose=False):
+    """Validate schema file"""
 
     if not schema:
         schema = get_github_masterschema()
@@ -88,11 +91,11 @@ def validate_schema(schema='', verbose=False):
     if results:
         return (False, results)
 
-    return (True, '')
+    return (True, "")
 
 
-def validate_jsongraph(jsongraph, schema='', verbose=False):
-    '''Validate JSON Graph against given jsongraph object and schema object'''
+def validate_jsongraph(jsongraph, schema="", verbose=False):
+    """Validate JSON Graph against given jsongraph object and schema object"""
 
     jg = get_json(jsongraph)
 
@@ -102,15 +105,19 @@ def validate_jsongraph(jsongraph, schema='', verbose=False):
         schema = get_json(schema)
 
     if not jg:
-        sys.exit('JSON Graph parameter does not appear to be a file object, filepath or JSON string.')
+        sys.exit(
+            "JSON Graph parameter does not appear to be a file object, filepath or JSON string."
+        )
     if not schema:
-        sys.exit('JSON Graph Schema parameter does not appear to be a file object, filepath or JSON string.')
+        sys.exit(
+            "JSON Graph Schema parameter does not appear to be a file object, filepath or JSON string."
+        )
 
     schema = Draft4Validator(schema)  # transform schema in a Schema validation object
 
     errors = [error for error in schema.iter_errors(jg)]
     if verbose and errors:
-        print('Problem with JSON Graph')
+        print("Problem with JSON Graph")
         for error in errors:
             print(error)
 
@@ -122,30 +129,30 @@ def validate_jsongraph(jsongraph, schema='', verbose=False):
     if errors:
         return errors
 
-    return (True, '')
+    return (True, "")
 
 
-def load_graphs(jsongraphs, validate=False, schema='', verbose=False):
-    '''Loads one or more graphs from jsongraphs JSON as a generator'''
+def load_graphs(jsongraphs, validate=False, schema="", verbose=False):
+    """Loads one or more graphs from jsongraphs JSON as a generator"""
 
     jgs = get_json(jsongraphs)
 
     if validate:
         (status, results) = validate_jsongraph(jsongraphs, schema, verbose)
-        sys.exit('JSON Graph does not validate')
+        sys.exit("JSON Graph does not validate")
 
-    if 'graph' in jgs:
-        yield jgs['graph']
+    if "graph" in jgs:
+        yield jgs["graph"]
 
-    if 'graphs' in jgs:
-        for graph in jgs['graphs']:
+    if "graphs" in jgs:
+        for graph in jgs["graphs"]:
             yield graph
 
 
 def test_example_graphs():
-    '''Test and usage example'''
-    single_graph_link = 'https://raw.githubusercontent.com/jsongraph/json-graph-specification/master/examples/usual_suspects.json'
-    multiple_graph_link = 'https://raw.githubusercontent.com/jsongraph/json-graph-specification/master/examples/car_graphs.json'
+    """Test and usage example"""
+    single_graph_link = "https://raw.githubusercontent.com/jsongraph/json-graph-specification/master/examples/usual_suspects.json"
+    multiple_graph_link = "https://raw.githubusercontent.com/jsongraph/json-graph-specification/master/examples/car_graphs.json"
 
     f = urllib.request.urlopen(single_graph_link)
     sg = json.load(f)
@@ -156,24 +163,24 @@ def test_example_graphs():
     f.close
 
     print("Does JSON Graph Schema validate?")
-    validate_schema(schema='', verbose=True)
+    validate_schema(schema="", verbose=True)
 
     print("\nDoes Single Graph example validate?")
-    validate_jsongraph(sg, schema='', verbose=True)
+    validate_jsongraph(sg, schema="", verbose=True)
 
     print("\nShow Label of Single Graph")
-    graphs = load_graphs(sg, validate=False, schema='', verbose=False)
-    print("    Label: ", next(graphs)['label'])
+    graphs = load_graphs(sg, validate=False, schema="", verbose=False)
+    print("    Label: ", next(graphs)["label"])
 
     print("\nShow Label's of Multiple Graphs")
-    graphs = load_graphs(mg, validate=False, schema='', verbose=False)
+    graphs = load_graphs(mg, validate=False, schema="", verbose=False)
     for graph in graphs:
-        print("    Label: ", graph['label'])
+        print("    Label: ", graph["label"])
 
 
 def main():
     test_example_graphs()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
